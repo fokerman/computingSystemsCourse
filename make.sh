0b7b6c138a738f37f498a5bdb2bfff1bd02d8d3c
David Alejandro Gonzalez Marquez <dmarquez@dc.uba.ar>
2022-03-13 23:19:46 -0300

    adding files to make all lessons

---
+++ make.sh
@@ -0,0 +1,42 @@
+cd T01_Representacion_de_la_Informacion_Parte_1
+make
+cd -
+cd T02_Representacion_de_la_Informacion_Parte_2
+make
+cd -
+cd T03_Algebra_de_boole_y_Compuertas
+make
+cd -
+cd T04_Circuitos_Combinatorios
+make
+cd -
+cd T05_Circuitos_Secuenciales
+make
+cd -
+cd T06_Maquina_Simple
+make
+cd -
+cd T07_Arquitectura_de_procesadores
+make
+cd -
+cd T08_Lenguaje_Ensamblador
+make
+cd -
+cd T09_Memorias
+make
+cd -
+cd T10_Hardware_de_Entrada_Salida
+make
+cd -
+cd T11_Sistemas_de_Computo_y_su_evolucion
+make
+cd -
+cd T12_Programacion_en_C
+make
+cd -
+cd T13_Memoria_dinamica
+make
+cd -
+cd T14_Compilador_Ensamblador_Linker
+make
+cd -
