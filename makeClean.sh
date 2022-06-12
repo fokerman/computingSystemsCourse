84fc2ff0806da8f24a75512e1c7d4a065864eadf
David Alejandro Gonzalez Marquez <dmarquez@dc.uba.ar>
2022-06-12 01:48:50 -0300

    update lesson memory manager

--- makeClean.sh
+++ makeClean.sh
@@ -1,57 +1,13 @@
-cd T01_Representacion_de_la_Informacion_Parte_1
-make cleanall
-cd -
-cd T02_Representacion_de_la_Informacion_Parte_2
-make cleanall
-cd -
-cd T03_Algebra_de_boole_y_Compuertas
-make cleanall
-cd -
-cd T04_Circuitos_Combinatorios
-make cleanall
-cd -
-cd T05_Circuitos_Secuenciales
-make cleanall
-cd -
-cd T06_Maquina_Simple
-make cleanall
-cd -
-cd T07_Arquitectura_de_procesadores
-make cleanall
-cd -
-cd T08_Lenguaje_Ensamblador
-make cleanall
-cd -
-cd T09_Memorias
-make cleanall
-cd -
-cd T10_Hardware_de_Entrada_Salida
-make cleanall
-cd -
-cd T11_Sistemas_de_Computo_y_su_evolucion
-make cleanall
-cd -
-cd T12_Programacion_en_C
-make cleanall
-cd -
-cd T13_Memoria_dinamica
-make cleanall
-cd -
-cd T14_Compilador_Ensamblador_Linker
-make cleanall
-cd -
-cd T15_Introduccion_a_los_Sistemas_Operativos
-make cleanall
-cd -
-cd T16_Procesos
-make cleanall
-cd -
-cd T17_Planificacion_de_Procesos
-make cleanall
-cd -
-cd T18_Sincronizacion_de_Procesos
-make cleanall
-cd -
-cd T19_Administracion_de_Memoria
-make cleanall
-cd -
+#!/bin/bash
+
+# find folders
+folders=$(find T* -maxdepth 0 -type d)
+
+# remove results
+rm -fr published
+
+# remove all
+for x in $folders
+do
+    make -C $x cleanall
+done
